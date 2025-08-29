14ff274590e8c88e896326b01c1472ba006be9af
WellWellGood <wellwellgood@example.com>
2025-08-29 16:45:16 +0900

    Toss

--- src/pages/benefit/Imageimport.jsx
+++ src/pages/benefit/Imageimport.jsx
@@ -1,62 +1,52 @@
-import ticket from './img/1000ticket.jpg';
-import bag from './img/bag.jpg';
-import bell from './img/bell.jpg';
-import button from './img/button.jpg';
-import card2 from './img/card2.jpg';
-import card3 from './img/card3.jpg';
-import card4 from './img/card4.jpg';
-import clova from './img/clova.jpg';
-import coin from './img/coin.jpg';
-import dart from './img/dart.jpg';
-import grape from './img/grape.jpg';
-import house1 from './img/house1.jpg';
-import house2 from './img/house2.jpg';
-import house3 from './img/house3.jpg';
-import house4 from './img/house4.jpg';
-import Live from './img/Live.jpg';
-import moneybag from './img/moneybag.jpg';
-import monreycelender from './img/monreycelender.jpg';
-import monster from './img/monster.jpg';
-import pin from './img/pin.jpg';
-import point from './img/point.jpg';
-import puzzle from './img/puzzle.jpg';
-import Qbox from './img/Qbox.jpg';
-import star from './img/star.jpg';
-import stemper from './img/stemper.jpg';
-import ticketpercent from './img/ticketpercent.jpg';
-import ticket2 from './img/ticket2.jpg';
-import ticket3 from './img/ticket3.jpg';
-import webtoon from './img/webtoon.jpg';
+import bag from './img/bag.png';
+import bell from './img/bell.png';
+import button from './img/button.png';
+import clova from './img/clova.png';
+import dart from './img/dart.png';
+import grape from './img/grape.png';
+import moneybag from './img/moneybag.png';
+import monster from './img/monster.png';
+import star from './img/star.png';
+import stemper from './img/stemper.png';
+import ticket2 from './img/ticket2.png';
+import ticket3 from './img/ticket3.png';
+import webtoon from './img/webtoon.png';
+import bacground from './img/background.png';
+import chiken from './img/chiken.png';
+import giftbox from './img/giftbox.png';
+import goldcard from './img/goldcard.png';
+import map from './img/map.png';
+import money from './img/money.png';
+import monetbag from './img/moneybag.png';;
+import tree from './img/tree.png';
+import wonball from './img/wonball.png';
+import cardphone from './img/cardphone.png';
+
 
 
 export const image = {
-    ticket,
     bag,
     bell,
     button,
-    card2,
-    card3,
-    card4,
     clova,
-    coin,
     dart,
     grape,
-    house1,
-    house2,
-    house3,
-    house4,
-    Live,
     moneybag,
-    monreycelender,
     monster,
-    pin,
-    point,
-    puzzle,
-    Qbox,
     star,
     stemper,
-    ticketpercent,
     ticket2,
     ticket3,
     webtoon,
+    bacground,
+    chiken,
+    giftbox,
+    goldcard,
+    map,
+    money,
+    monetbag,
+    monster,
+    tree,
+    wonball,
+    cardphone
 }
--- src/pages/benefit/rewardItem.jsx
+++ src/pages/benefit/rewardItem.jsx
@@ -1,99 +1,87 @@
 export const rewardItems = [
-    {
+    {//
         title: "오늘의 행운복권",
         reward: "포인트 받기",
         to: "/",
         key: "clova", // ← iconImages에서 대응되는 이미지 키
     },
     {
-        title: "라이브 쇼핑",
-        reward: "포인트 받기",
-        to: "/",
-        key: "Live",
-    },
-    {
-        title: "행운퀴즈",
-        reward: "추가 혜택 보기",
-        to: "/",
-        key: "Qbox",//여기서부터 이미지명 변경
-    },
-    {
-        title: "이본 주 미션",
+        title: "이번수 미션",
         reward: "얼마 받을지 보기",
         to: "/",
-      key: "dart", // ← iconImages에서 대응되는 이미지 키
+        key: "dart",
     },
     {
-        title: "두근두근 1등 찍기",
+        title: "두근두근 1증 찍기",
         reward: "포인트 받기",
         to: "/",
-        key: "stemper",
+        key: "stemper",//여기서부터 이미지명 변경
     },
-    {
+    {//
         title: "일주일 방문 미션",
         reward: "포인트 받기",
         to: "/",
-        key: "star",
+      key: "star", // ← iconImages에서 대응되는 이미지 키
     },
     {
         title: "머니 알림",
         reward: "포인트 받기",
         to: "/",
-      key: "bell", // ← iconImages에서 대응되는 이미지 키
+        key: "bell",
     },
     {
-        title: "200원 퍼즐 미션",
-        reward: "도전하기",
-        to: "/",
-        key: "puzzle",
-    },
-    {
-        title: "계좌 새로 만들고",
+        title: "오늘의 금융미션하고",
         reward: "포인트 받기",
         to: "/",
-        key: "monreycelender",
+        key: "moneybag",
     },
     {
         title: "포도알 완성하고",
-        reward: "1,000원 받기",
+        reward: "1000원 받기",
         to: "/",
-      key: "grape", // ← iconImages에서 대응되는 이미지 키
+        key: "grape", // ← iconImages에서 대응되는 이미지 키
     },
     {
-        title: "주유비 결제내역 있다면",
-        reward: "랜덤 포인트 받기",
+        title: "현금 700만원 이벤트 보고",
+        reward: "포인트 받기",
         to: "/",
-        key: "card2",
+        key: "money",
     },
     {
-        title: "대출 능력 확인하고",
-        reward: "100원 이상 받기",
+        title: "치킨 100원 이벤트 보고",
+        reward: "포인트 받기",
         to: "/",
-        key: "card2",
-    },
-    {
-        title: "토스에서 대리운전 쿠폰",
-        reward: "8,000원 받기",
-        to: "/facepay",
-      key: "ticketpercent", // ← iconImages에서 대응되는 이미지 키
+        key: "chiken",
     },
     {
         title: "쿠폰 혜택 20초 구경하고",
-        reward: "포인트 받기",
+        reward: "포인트 받기기",
         to: "/",
         key: "ticket2",
     },
     {
-        title: "오늘의 카드 찾고",
-        reward: "3원 받기",
-        to: "/card-event",
-        key: "card3",
+        title: "무료 배달 서비스 써보고",
+        reward: "배달 쿠폰 3장 받기기",
+        to: "/",
+        key: "ticket3",
+    },
+    {
+        title: "자동차 미션하고",
+        reward: "리워드 받기",
+        to: "/",
+        key: "wonball",
     },
     {
         title: "내 카드 한 번만 연결하고",
         reward: "500원 받기",
+        to: "/facepay",
+        key: "cardphone",
+    },
+    {
+        title: "같은 그림 3개 맟추고고",
+        reward: "포인트 받기",
         to: "/",
-      key: "coin", //////////////////////////////////////////////////////////
+        key: "bacground",
     },
     {
         title: "토스쇼핑 구경하고",
@@ -102,75 +90,27 @@ export const rewardItems = [
         key: "bag",
     },
     {
-        title: "월 8천원 요금제 쓰고",
-        reward: "5천원 받기",
-        to: "/card-event",
-        key: "ticket",
-    },
-    {
-        title: "부동산 상식 퀴즈 풀고",
-        reward: "10원 받기",
-        to: "/",
-      key: "Qbox", // ← iconImages에서 대응되는 이미지 키
-    },
-    {
         title: "해달과 조개 모으고",
         reward: "포인트 받기",
         to: "/",
         key: "monster",
     },
     {
-        title: "놓친 구폰 알림 받고",
-        reward: "포인트 받기",
-        to: "/card-event",
-        key: "ticket3",
-    },
-    {
-        title: "페이스페이 체험단",
-        reward: "시작하고 10원 받기",
+        title: "새 모바일 카드 만들고",
+        reward: "토스멤버십 무료로 쓰기",
         to: "/",
-      key: "coin", // ← iconImages에서 대응되는 이미지 키
+        key: "goldcard",
     },
     {
-        title: "토스페이 고르고",
+        title: "지도 구결하고",
         reward: "5원 받기",
         to: "/",
-        key: "pin",
-    },
-    {
-        title: "카드값 이벤트 구경하고",
-        reward: "3원 받기",
-        to: "/card-event",
-        key: "card4",
-    },
-    {
-        title: "내 집 등록하고",
-        reward: "10원 받기",
-        to: "/",
-      key: "house1", // ← iconImages에서 대응되는 이미지 키
-    },
-    {
-        title: "종합부동산세 환급 알아보고",
-        reward: "10원 받기",
-        to: "/",
-        key: "house2",
-    },
-    {
-        title: "브랜드 구경하고",
-        reward: "포인트 받기",
-        to: "/",
-        key: "house3",
-    },
-    {
-        title: "인테이러 상담하고",
-        reward: "5원 받기",
-        to: "/",
-      key: "house4", // ← iconImages에서 대응되는 이미지 키
+        key: "map",
     },
     {
         title: "웹툰 보고",
         reward: "포인트 받기",
-        to: "/tosspay",
+        to: "/",
         key: "webtoon",
     },
     {
@@ -178,5 +118,11 @@ export const rewardItems = [
         reward: "10원 받기",
         to: "/",
         key: "button",
+    },
+    {
+        title: "진행중인 이벤트",
+        reward: "모아보기",
+        to: "/",
+        key: "giftbox",
     },
 ];
