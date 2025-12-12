300b02bda5e97ad4acdac167f44b066df9e5c2f0
WellWellGood <wellwellgood@example.com>
2025-12-12 15:58:50 +0900

    Toss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default { server: { proxy: { '/api': { target: 'http://localhost:8080', changeOrigin: true } } } };
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        fs: {
+            strict: false
+        }
+    }
+})
